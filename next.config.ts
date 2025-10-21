2fd0a64f7b6bbb474024c18757e60f2039527a15
leo-garcia-xquisito <leonardogarcia@xquisito.ai>
2025-10-21 10:15:53 -0600

    redireccion a flexbill

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,9 @@ import type { NextConfig } from "next";
 import withPWA from "next-pwa";
 
 const nextConfig: NextConfig = {
+  basePath: "/flexbill",
+  assetPrefix: "/flexbill",
+  trailingSlash: true,
   typescript: {
     ignoreBuildErrors: true,
   },
